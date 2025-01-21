fea2dfee8b9d596b49e1348da920e62fdbfb2144
Vikraam17 <vikyvino1773@gmail.com>
2025-01-21 12:50:36 +0530

    js added

---
+++ javascript/variable.js
@@ -0,0 +1,23 @@
+var a;
+console.log(typeof a) // functional scope
+
+let b;
+b=20
+console.log(typeof b) //block scope
+
+const c="Hello";
+console.log(typeof c) //block scope
+
+const t =true;
+console.log(typeof t)
+
+var a = {
+  firstName:"raja",
+  Class:`ECE A`
+}
+console.log(typeof a)
+
+console.log(b-a)
+
+var a = null;
+console.log(typeof a)
